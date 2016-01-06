9e4eaad1aee1ce948a6e4e6cb817caad61e37fa3
Jupiter2359media <cao.vu@2359media.com.vn>
2016-01-06 13:25:35 +0700

    [ADD] Add initial app.

---
+++ app/src/main/java/com/hasbrain/milestonetest/model/FacebookImage.java
@@ -0,0 +1,61 @@
+package com.hasbrain.milestonetest.model;
+
+/**
+ * Created by Jupiter (vu.cao.duy@gmail.com) on 1/5/16.
+ */
+public class FacebookImage {
+    private String id;
+    private String name;
+    private String imageUrl;
+    private String thumbnailUrl;
+    private String createdTime;
+    private String fromUserName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getFromUserName() {
+        return fromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        this.fromUserName = fromUserName;
+    }
+}
